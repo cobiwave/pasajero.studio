4ca788a81072dc9ff28bf7742b00990b747d7bd7
Cobi <jacobojavier98@gmail.com>
2025-01-18 01:43:09 -0300

    add initial GraphQL queries file

---
+++ src/graphql/queries.ts
@@ -0,0 +1,9 @@
+import { graphql } from '@/lib/datocms/graphql';
+
+// export const ALL_ARTICLES_PATHS = graphql(/* GraphQL */ `
+//   query AllArticlesPaths {
+//     allArticles {
+//       slug
+//     }
+//   }
+// `);
