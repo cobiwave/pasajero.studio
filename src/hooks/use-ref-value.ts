144f9e24c3e630ac7ff3301c7b67997b88f7da83
Cobi <jacobojavier98@gmail.com>
2025-03-26 18:24:45 -0300

    Add custom hooks for managing refs and transition presence; remove unused video hooks

---
+++ src/hooks/use-ref-value.ts
@@ -0,0 +1,15 @@
+import { useRef } from 'react';
+/**
+ * Keeps a ref up to date with a changing value.
+ *
+ * Normal values are captured in scopes, while refs are not. Therefore a changing value in a callback
+ * requires the use of a ref.
+ *
+ * @param value The value to keep in the ref.
+ * @returns A ref object that is updated with the value.
+ */
+export function useRefValue<T>(value: T) {
+  const ref = useRef(value);
+  ref.current = value;
+  return ref;
+}
