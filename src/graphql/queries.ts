866e4f7535d40685531492d85e9e10554061b597
Jacobo Martinez <jacobolm@amazon.com>
2025-07-29 19:03:35 -0300

    Add getPageData function to fetch page data with C01ImageGallery components

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,4 +1,11 @@
+import type { DatoGetPageData } from '@/data/types';
+
+import { DEBUG } from '@/data/constants';
+
+import { C01ImageGalleryBlockFragment } from '@/components/C01ImageGallery/C01ImageGallery.fragment';
+
 import { TagFragment } from '@/lib/datocms/commonFragments';
+import { executeQuery } from '@/lib/datocms/executeQuery';
 import { graphql } from '@/lib/datocms/graphql';
 
 import { ResponsiveImageFragment } from './fragments/FragmentResponsiveImage';
@@ -56,3 +63,34 @@ export const QueryArticle = graphql(
   `,
   [TagFragment, ResponsiveImageFragment]
 );
+
+export const getPageData = async (pageName: string = 'home-page'): Promise<DatoGetPageData> => {
+  const query = graphql(
+    `
+      query MyQuery($slug: String!) {
+        page(filter: { slug: { eq: $slug } }) {
+          id
+          slug
+          title
+          components {
+            ...C01ImageGalleryBlockFragment
+          }
+        }
+      }
+    `,
+    [C01ImageGalleryBlockFragment]
+  );
+
+  const page = await executeQuery(query, {
+    variables: {
+      slug: pageName
+    }
+  });
+
+  if (DEBUG) {
+    // eslint-disable-next-line no-console
+    console.log('{{ getPageData fn }}', page);
+  }
+
+  return page as unknown as DatoGetPageData;
+};
