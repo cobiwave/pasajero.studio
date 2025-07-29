d276e9526f16d7e11b91553ee58ef2bf904af118
Jacobo Martinez <jacobolm@amazon.com>
2025-07-29 19:01:38 -0300

    Refactor Home page to fetch and render components dynamically based on category data

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,3 +1,29 @@
-export default async function Page() {
-  return <h1>Category Page</h1>;
+import type { PageProps } from '@/data/types';
+import type { ComponentList } from '@/renderComponent';
+
+import { notFound } from 'next/navigation';
+
+import { getPageData } from '@/graphql/queries';
+import RenderComponents from '@/renderComponent';
+
+export default async function Page({ params }: PageProps) {
+  const { category } = params;
+
+  const { page } = await getPageData(category);
+
+  const components =
+    page?.components.map((component) => {
+      const { __typename } = component;
+
+      return {
+        __typename,
+        props: { data: component }
+      };
+    }) || [];
+
+  if (!page) {
+    notFound();
+  }
+
+  return <RenderComponents components={components as ComponentList[]} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,32 @@
+import type { ComponentList } from '@/renderComponent';
+
 import { notFound } from 'next/navigation';
 
-import { PageHome } from '@/components/PageHome';
-
-import { ImageGalleryBlockFragment } from '@/graphql/fragments/FragmentImageGallery';
-import { QueryAllArticles } from '@/graphql/queries';
-import { TagFragment } from '@/lib/datocms/commonFragments';
-import { executeQuery } from '@/lib/datocms/executeQuery';
-import { generateMetadataFn } from '@/lib/datocms/generateMetadataFn';
-import { graphql, type TypeFromQuery } from '@/lib/datocms/graphql';
-
-const query = graphql(
-  /* GraphQL */ `
-    query HomePageQuery {
-      homePage {
-        _seoMetaTags {
-          ...TagFragment
-        }
-        imageGallery {
-          ...ImageGalleryBlockFragment
-        }
-      }
-    }
-  `,
-  [TagFragment, ImageGalleryBlockFragment]
-);
-
-/**
- * We use a helper to generate function that fits the Next.js
- * `generateMetadata()` format, automating the creation of meta tags based on
- * the `_seoMetaTags` present in a DatoCMS GraphQL query.
- */
-export const generateMetadata = generateMetadataFn({
-  query,
-  // A callback that picks the SEO meta tags from the result of the query
-  pickSeoMetaTags: (data) => data.homePage?._seoMetaTags
-});
+import { getPageData } from '@/graphql/queries';
+import RenderComponents from '@/renderComponent';
 
 export default async function Home() {
-  const { homePage } = await executeQuery(query);
-  const { allArticles }: TypeFromQuery<typeof QueryAllArticles> = await executeQuery(QueryAllArticles);
+  const { page } = await getPageData('home-page');
+  // const { allArticles }: TypeFromQuery<typeof QueryAllArticles> = await executeQuery(QueryAllArticles);
 
-  if (!homePage) {
+  const components =
+    page?.components.map((component) => {
+      const { __typename } = component;
+
+      return {
+        __typename,
+        props: { data: component }
+      };
+    }) || [];
+
+  if (!page) {
     notFound();
   }
 
   return (
-    <PageHome
-      content={{
-        homePage,
-        allArticles
-      }}
-    />
+    <>
+      {/* {allArticles.length > 0 ? <ArticleList articles={allArticles} /> : null} */}
+      <RenderComponents components={components as ComponentList[]} />
+    </>
   );
 }
