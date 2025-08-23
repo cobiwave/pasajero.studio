3e3f1389623495ff58dafc3bfabe32fb440b63ad
Cobi <jacobojavier98@gmail.com>
2025-08-23 00:23:05 -0300

    fix: add revalidate constant to page for caching control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { notFound } from 'next/navigation';
 import { getPageData } from '@/graphql/queries';
 import RenderComponents from '@/renderComponent';
 
+export const revalidate = 0;
+
 export default async function Home() {
   const { page } = await getPageData('home-page');
   // const { allArticles }: TypeFromQuery<typeof QueryAllArticles> = await executeQuery(QueryAllArticles);
