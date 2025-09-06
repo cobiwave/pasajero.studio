3e7bbf0f20940b78fa65eff3b530c7d9a57b31f3
Cobi <jacobojavier98@gmail.com>
2025-09-06 02:11:33 -0300

    feat: add PageHome component with controller and view structure

---
+++ src/components/PageHome/PageHome.controller.tsx
@@ -0,0 +1,19 @@
+import type { FC } from 'react';
+import type { QueryAllArticles } from '@/graphql/queries/articles';
+import type { TypeFromQuery } from '@/lib/datocms/graphql';
+import type { ComponentList } from '@/renderComponent';
+
+import { memo } from 'react';
+
+import { View } from './PageHome.view';
+
+export interface ControllerProps {
+  articles: TypeFromQuery<typeof QueryAllArticles>['allArticles'];
+  components: ComponentList[];
+}
+
+export const Controller: FC<ControllerProps> = memo((props) => {
+  return <View {...props} />;
+});
+
+Controller.displayName = 'PageHome_Controller';
