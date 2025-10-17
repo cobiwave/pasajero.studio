349108ec223ab8d0e7a5929fef0f2395aefb7e67
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2025-10-17 18:46:56 -0300

    feat: replace ResponsiveImage with Image component for featured image rendering in PageArticle

--- src/components/pages/PageArticle/PageArticle.view.tsx
+++ src/components/pages/PageArticle/PageArticle.view.tsx
@@ -7,18 +7,21 @@ import { useMemo } from 'react';
 import classNames from 'classnames';
 import { animate } from 'motion';
 
+import { AspectRatio } from '@/data/constants';
+
 import { prettifyDate } from '@/utils/basic-functions';
 
 import { useRefs } from '@/hooks/use-refs';
 import { useTransitionPresence } from '@/hooks/use-transition-presence';
 
-import { RichTextBlockFragment } from '@/graphql/components/RichText.fragment';
+import { Image } from '@/components/atoms/Image';
+import { ImageBlockFragment } from '@/components/atoms/Image/Image.controller';
+import { RichText } from '@/components/RichText';
+
+import { RichTextBlockFragment } from '@/graphql/components/RichTextBlock.fragment';
 import { readFragment } from '@/lib/datocms/graphql';
 
 import css from './PageArticle.module.scss';
-
-import ResponsiveImage from '../../atoms/ResponsiveImage/ResponsiveImage';
-import { RichText } from '../../RichText';
 
 export interface ViewProps extends ControllerProps {}
 
@@ -28,6 +31,8 @@ export type ViewRefs = {
 
 export const View: FC<ViewProps> = ({ article }) => {
   const unmaskedRichText = readFragment(RichTextBlockFragment, article?.richText);
+  const unmaskedFeaturedImage = readFragment(ImageBlockFragment, article?.featuredImage);
+
   const refs = useRefs<ViewRefs>();
 
   useTransitionPresence(
@@ -44,25 +49,12 @@ export const View: FC<ViewProps> = ({ article }) => {
     <section className={classNames('PageArticle', css.root)} ref={refs.root}>
       <div className={css.heading}>
         {article?.title ? <h1 className={css.title}>{article.title}</h1> : null}
-        {article?.articleDate ? <div className={css.date}>{prettifyDate(article.articleDate)}</div> : null}
+        {article?.date ? <div className={css.date}>{prettifyDate(article.date)}</div> : null}
       </div>
 
-      <div className={css.banner}>
-        {article?.featuredImage?.responsiveImage ? (
-          <ResponsiveImage
-            imgStyle={{
-              width: '100%',
-              height: '100%',
-              maxWidth: '100%',
-              objectFit: 'cover',
-              aspectRatio: '16 / 9'
-            }}
-            pictureClassName={css.image}
-            data={article.featuredImage.responsiveImage}
-          />
-        ) : null}
+      <div className={css.featuredImage}>
+        {unmaskedFeaturedImage ? <Image data={unmaskedFeaturedImage} aspectRatio={AspectRatio.SixteenNine} /> : null}
       </div>
-
       {unmaskedRichText ? <RichText data={unmaskedRichText} /> : null}
     </section>
   );
