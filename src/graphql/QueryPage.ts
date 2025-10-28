ba70b613e8c1d494da6e4a9409e502d1ff6b68b5
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2025-10-28 15:38:37 -0300

    feat: refactor QueryPage to include locale parameter in GraphQL query

--- src/graphql/QueryPage.ts
+++ src/graphql/QueryPage.ts
@@ -1,42 +1,23 @@
-import type { DatoGetPageData } from '@/data/types';
-
-import { DEBUG } from '@/data/constants';
-
 import { ImageGalleryBlockFragment } from '@/components/ImageGallery/ImageGallery.controller';
 
-import { executeQuery } from '@/lib/datocms/executeQuery';
 import { graphql } from '@/lib/datocms/graphql';
 
 import { RichTextBlockFragment } from './components/RichTextBlock.fragment';
 
-export const getPageData = async (pageName: string): Promise<DatoGetPageData> => {
-  const query = graphql(
-    `
-      query getPage($slug: String!) {
-        page(filter: { slug: { eq: $slug } }) {
-          id
-          slug
-          title
-          components {
-            ...ImageGalleryBlockFragment
-            ...RichTextBlockFragment
-          }
+export const QueryPage = graphql(
+  /* GraphQL */
+  `
+    query getPage($locale: SiteLocale, $slug: String!) {
+      page(locale: $locale, filter: { slug: { eq: $slug } }) {
+        id
+        slug
+        title
+        components {
+          ...ImageGalleryBlockFragment
+          ...RichTextBlockFragment
         }
       }
-    `,
-    [ImageGalleryBlockFragment, RichTextBlockFragment]
-  );
-
-  const page = await executeQuery(query, {
-    variables: {
-      slug: pageName
     }
-  });
-
-  if (DEBUG) {
-    // eslint-disable-next-line no-console
-    console.log('{{ getPageData fn }}', page);
-  }
-
-  return page as unknown as DatoGetPageData;
-};
+  `,
+  [ImageGalleryBlockFragment, RichTextBlockFragment]
+);
